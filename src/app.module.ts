03a768cf469925a54199bb050bae8db335e39fe4
nofamex <nofaldi.atmam@tiket.com>
2022-05-13 20:27:15 +0700

    feat: auth service init

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { Auth } from './auth/schema/auth.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      database: 'postgres',
+      username: 'postgres',
+      password: null,
+      entities: [Auth],
+      synchronize: true,
+    }),
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
