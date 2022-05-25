539a3a05c0fccc6c2716b618c37309ca13f6ee24
nofamex <nofaldi.atmam@tiket.com>
2022-05-25 22:08:13 +0700

    feat: register merch

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,9 +10,6 @@ import { Auth } from './auth/schema/auth.entity';
       url: process.env.DATABASE_URL,
       entities: [Auth],
       synchronize: true,
-      ssl: {
-        rejectUnauthorized: false,
-      },
     }),
     AuthModule,
   ],
